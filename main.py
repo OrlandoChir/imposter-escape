b91b803c74605fef7f60d8e64275d4d2b714022f
Orlando Chirinos <153592294+OrlandoChir@users.noreply.github.com>
2025-10-25 19:02:41 +0200

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,23 @@
+player_1 = sprites.create(img("""
+        .....fffffff.....
+        ...ff2222222ff...
+        ..f22222222222f..
+        .f222fffffff222f.
+        .f22f9991119f22f.
+        f22f999991199f22f
+        f22f999999919f22f
+        f22f999999999f22f
+        f22f999999999f22f
+        f22f999999999f22f
+        f222ff99999ff222f
+        f22222fffff22222f
+        f222222222222222f
+        f222222222222222f
+        f222222222222222f
+        .f222222f222222f.
+        .f222222f222222f.
+        .f222222f222222f.
+        .f222222f222222f.
+        .fffffffffffffff.
+        """),
+    SpriteKind.player)
