379e5b2d4f040860f204d1f45237f96014724a07
Orlando Chirinos <153592294+OrlandoChir@users.noreply.github.com>
2025-11-04 00:43:06 +0100

    Begin function of menu

--- main.py
+++ main.py
@@ -2,7 +2,14 @@
 class SpriteKind:
     Dialogue = SpriteKind.create()
 
+def on_on_created(sprite):
+    sprite.follow(player_1, 30)
+sprites.on_created(SpriteKind.enemy, on_on_created)
+
 def on_up_pressed():
+    global y_direction, x_direction
+    y_direction = -200
+    x_direction = 0
     animation.run_image_animation(player_1,
         assets.animation("""
             walk_up_animation
@@ -11,14 +18,45 @@ def on_up_pressed():
         True)
 controller.up.on_event(ControllerButtonEvent.PRESSED, on_up_pressed)
 
-def on_overlap_tile(sprite, location):
+def initialize_menu():
+    pass
+
+def on_overlap_tile(sprite2, location):
     tileUtil.load_connected_map(MapConnectionKind.door1)
     tiles.place_on_random_tile(player_1, tileUtil.door0)
     if tileUtil.current_tilemap() == map_zone_2:
+        info.start_countdown(300)
         player_1.x = 26
     else:
         player_1.x = 231
 scene.on_overlap_tile(SpriteKind.player, tileUtil.door0, on_overlap_tile)
+
+def on_a_pressed():
+    global bullet
+    bullet = sprites.create_projectile_from_sprite(img("""
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . 4 4 . . . . . . .
+            . . . . . . 4 5 5 4 . . . . . .
+            . . . . . . 2 5 5 2 . . . . . .
+            . . . . . . . 2 2 . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            """),
+        player_1,
+        x_direction,
+        y_direction)
+    music.play(music.melody_playable(music.pew_pew),
+        music.PlaybackMode.UNTIL_DONE)
+controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
 
 def on_down_released():
     animation.stop_animation(animation.AnimationTypes.ALL, player_1)
@@ -28,6 +66,9 @@ def on_down_released():
 controller.down.on_event(ControllerButtonEvent.RELEASED, on_down_released)
 
 def on_left_pressed():
+    global y_direction, x_direction
+    y_direction = 0
+    x_direction = -200
     animation.run_image_animation(player_1,
         assets.animation("""
             WalkLeft
@@ -35,6 +76,13 @@ def on_left_pressed():
         200,
         True)
 controller.left.on_event(ControllerButtonEvent.PRESSED, on_left_pressed)
+
+def on_on_overlap(sprite3, otherSprite):
+    sprites.destroy(sprite3, effects.spray, 500)
+    sprites.destroy(bullet, effects.none, 0)
+    music.play(music.melody_playable(music.zapped),
+        music.PlaybackMode.UNTIL_DONE)
+sprites.on_overlap(SpriteKind.enemy, SpriteKind.projectile, on_on_overlap)
 
 def on_right_released():
     animation.stop_animation(animation.AnimationTypes.ALL, player_1)
@@ -50,7 +98,17 @@ def on_left_released():
         """))
 controller.left.on_event(ControllerButtonEvent.RELEASED, on_left_released)
 
+def on_countdown_end():
+    game.game_over(True)
+    game.set_game_over_effect(True, effects.slash)
+    game.set_game_over_message(True, "YOU SURVIVED")
+    music.stop_all_sounds()
+info.on_countdown_end(on_countdown_end)
+
 def on_right_pressed():
+    global y_direction, x_direction
+    y_direction = 0
+    x_direction = 200
     animation.run_image_animation(player_1,
         assets.animation("""
             WalkRight
@@ -59,6 +117,10 @@ def on_right_pressed():
         True)
 controller.right.on_event(ControllerButtonEvent.PRESSED, on_right_pressed)
 
+def on_on_overlap2(sprite4, otherSprite2):
+    info.change_life_by(-1)
+sprites.on_overlap(SpriteKind.enemy, SpriteKind.player, on_on_overlap2)
+
 def on_up_released():
     animation.stop_animation(animation.AnimationTypes.ALL, player_1)
     player_1.set_image(assets.image("""
@@ -66,7 +128,17 @@ def on_up_released():
         """))
 controller.up.on_event(ControllerButtonEvent.RELEASED, on_up_released)
 
+def on_map_loaded(tilemap2):
+    if tileUtil.current_tilemap() == map_zone_1:
+        tileUtil.cover_all_tiles(tileUtil.door0, sprites.dungeon.door_open_east)
+    else:
+        tileUtil.replace_all_tiles(tileUtil.door0, sprites.builtin.forest_tiles10)
+tileUtil.on_map_loaded(on_map_loaded)
+
 def on_down_pressed():
+    global y_direction, x_direction
+    y_direction = 200
+    x_direction = 0
     animation.run_image_animation(player_1,
         assets.animation("""
             walk_down_animation
@@ -75,7 +147,15 @@ def on_down_pressed():
         True)
 controller.down.on_event(ControllerButtonEvent.PRESSED, on_down_pressed)
 
-def on_overlap_tile2(sprite2, location2):
+def on_life_zero():
+    music.stop_all_sounds()
+    game.game_over(False)
+    music.play(music.string_playable("G F G A - F E D ", 120),
+        music.PlaybackMode.UNTIL_DONE)
+    game.set_game_over_message(False, "YOU GOT CAUGHT!")
+info.on_life_zero(on_life_zero)
+
+def on_overlap_tile2(sprite5, location2):
     global on_dialogue
     if controller.A.is_pressed():
         if current_zone == "mz_1":
@@ -98,10 +178,14 @@ scene.on_overlap_tile(SpriteKind.player,
     sprites.dungeon.chest_open,
     on_overlap_tile2)
 
+bullet: Sprite = None
+x_direction = 0
+y_direction = 0
 on_dialogue = False
 player_1: Sprite = None
 current_zone = ""
 map_zone_2: tiles.TileMapData = None
+map_zone_1: tiles.TileMapData = None
 map_zone_1 = tilemap("""
     mz_1
     """)
@@ -113,6 +197,7 @@ tiles.set_current_tilemap(map_zone_1)
 player_1 = sprites.create(assets.image("""
     walk_1
     """), SpriteKind.player)
+info.set_life(1)
 on_dialogue = False
 tileUtil.connect_maps(map_zone_1, map_zone_2, MapConnectionKind.door1)
 tiles.place_on_random_tile(player_1, sprites.dungeon.collectible_insignia)
@@ -127,9 +212,22 @@ def on_on_update():
         controller.move_sprite(player_1, 100, 100)
 game.on_update(on_on_update)
 
+def on_update_interval():
+    tileUtil.create_sprites_on_tiles(sprites.dungeon.collectible_red_crystal,
+        assets.image("""
+            Ghost
+            """),
+        SpriteKind.enemy)
+    tileUtil.create_sprites_on_tiles(sprites.dungeon.collectible_blue_crystal,
+        assets.image("""
+            Ghost
+            """),
+        SpriteKind.enemy)
+game.on_update_interval(5000, on_update_interval)
+
 def on_forever():
     scene.center_camera_at(0, 0)
     scene.camera_follow_sprite(player_1)
-    music.play(music.string_playable("C5 A B G A F G E ", 115),
+    music.play(music.string_playable("C5 A B G A F G E ", 147),
         music.PlaybackMode.UNTIL_DONE)
 forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,20 +1,23 @@
 namespace SpriteKind {
     export const Dialogue = SpriteKind.create()
 }
-sprites.onCreated(SpriteKind.Enemy, function (sprite) {
+
+sprites.onCreated(SpriteKind.Enemy, function on_on_created(sprite: Sprite) {
     sprite.follow(player_1, 30)
 })
-controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+controller.up.onEvent(ControllerButtonEvent.Pressed, function on_up_pressed() {
+    
     y_direction = -200
     x_direction = 0
-    animation.runImageAnimation(
-    player_1,
-    assets.animation`walk_up_animation`,
-    200,
-    true
-    )
+    animation.runImageAnimation(player_1, assets.animation`
+            walk_up_animation
+            `, 200, true)
 })
-scene.onOverlapTile(SpriteKind.Player, tileUtil.door0, function (sprite, location) {
+function initialize_menu() {
+    
+}
+
+scene.onOverlapTile(SpriteKind.Player, tileUtil.door0, function on_overlap_tile(sprite2: Sprite, location: tiles.Location) {
     tileUtil.loadConnectedMap(MapConnectionKind.Door1)
     tiles.placeOnRandomTile(player_1, tileUtil.door0)
     if (tileUtil.currentTilemap() == map_zone_2) {
@@ -23,106 +26,113 @@ scene.onOverlapTile(SpriteKind.Player, tileUtil.door0, function (sprite, locatio
     } else {
         player_1.x = 231
     }
+    
 })
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+controller.A.onEvent(ControllerButtonEvent.Pressed, function on_a_pressed() {
+    
     bullet = sprites.createProjectileFromSprite(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . 4 4 . . . . . . . 
-        . . . . . . 4 5 5 4 . . . . . . 
-        . . . . . . 2 5 5 2 . . . . . . 
-        . . . . . . . 2 2 . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `, player_1, x_direction, y_direction)
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . 4 4 . . . . . . .
+            . . . . . . 4 5 5 4 . . . . . .
+            . . . . . . 2 5 5 2 . . . . . .
+            . . . . . . . 2 2 . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            `, player_1, x_direction, y_direction)
     music.play(music.melodyPlayable(music.pewPew), music.PlaybackMode.UntilDone)
 })
-controller.down.onEvent(ControllerButtonEvent.Released, function () {
+controller.down.onEvent(ControllerButtonEvent.Released, function on_down_released() {
     animation.stopAnimation(animation.AnimationTypes.All, player_1)
-    player_1.setImage(assets.image`walk_down_1`)
+    player_1.setImage(assets.image`
+        walk_down_1
+        `)
 })
-controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+controller.left.onEvent(ControllerButtonEvent.Pressed, function on_left_pressed() {
+    
     y_direction = 0
     x_direction = -200
-    animation.runImageAnimation(
-    player_1,
-    assets.animation`WalkLeft`,
-    200,
-    true
-    )
+    animation.runImageAnimation(player_1, assets.animation`
+            WalkLeft
+            `, 200, true)
 })
-sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Projectile, function (sprite, otherSprite) {
-    sprites.destroy(sprite, effects.spray, 500)
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Projectile, function on_on_overlap(sprite3: Sprite, otherSprite: Sprite) {
+    sprites.destroy(sprite3, effects.spray, 500)
     sprites.destroy(bullet, effects.none, 0)
     music.play(music.melodyPlayable(music.zapped), music.PlaybackMode.UntilDone)
 })
-controller.right.onEvent(ControllerButtonEvent.Released, function () {
+controller.right.onEvent(ControllerButtonEvent.Released, function on_right_released() {
     animation.stopAnimation(animation.AnimationTypes.All, player_1)
-    player_1.setImage(assets.image`walk_right_1`)
+    player_1.setImage(assets.image`
+        walk_right_1
+        `)
 })
-controller.left.onEvent(ControllerButtonEvent.Released, function () {
+controller.left.onEvent(ControllerButtonEvent.Released, function on_left_released() {
     animation.stopAnimation(animation.AnimationTypes.All, player_1)
-    player_1.setImage(assets.image`walk_left_1`)
+    player_1.setImage(assets.image`
+        walk_left_1
+        `)
 })
-info.onCountdownEnd(function () {
+info.onCountdownEnd(function on_countdown_end() {
     game.gameOver(true)
     game.setGameOverEffect(true, effects.slash)
     game.setGameOverMessage(true, "YOU SURVIVED")
     music.stopAllSounds()
 })
-controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+controller.right.onEvent(ControllerButtonEvent.Pressed, function on_right_pressed() {
+    
     y_direction = 0
     x_direction = 200
-    animation.runImageAnimation(
-    player_1,
-    assets.animation`WalkRight`,
-    200,
-    true
-    )
+    animation.runImageAnimation(player_1, assets.animation`
+            WalkRight
+            `, 200, true)
 })
-sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function on_on_overlap2(sprite4: Sprite, otherSprite2: Sprite) {
     info.changeLifeBy(-1)
 })
-controller.up.onEvent(ControllerButtonEvent.Released, function () {
+controller.up.onEvent(ControllerButtonEvent.Released, function on_up_released() {
     animation.stopAnimation(animation.AnimationTypes.All, player_1)
-    player_1.setImage(assets.image`walk_up_1`)
+    player_1.setImage(assets.image`
+        walk_up_1
+        `)
 })
-tileUtil.onMapLoaded(function (tilemap2) {
+tileUtil.onMapLoaded(function on_map_loaded(tilemap2: tiles.TileMapData) {
     if (tileUtil.currentTilemap() == map_zone_1) {
         tileUtil.coverAllTiles(tileUtil.door0, sprites.dungeon.doorOpenEast)
     } else {
         tileUtil.replaceAllTiles(tileUtil.door0, sprites.builtin.forestTiles10)
     }
+    
 })
-controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+controller.down.onEvent(ControllerButtonEvent.Pressed, function on_down_pressed() {
+    
     y_direction = 200
     x_direction = 0
-    animation.runImageAnimation(
-    player_1,
-    assets.animation`walk_down_animation`,
-    200,
-    true
-    )
+    animation.runImageAnimation(player_1, assets.animation`
+            walk_down_animation
+            `, 200, true)
 })
-info.onLifeZero(function () {
+info.onLifeZero(function on_life_zero() {
     music.stopAllSounds()
     game.gameOver(false)
     music.play(music.stringPlayable("G F G A - F E D ", 120), music.PlaybackMode.UntilDone)
     game.setGameOverMessage(false, "YOU GOT CAUGHT!")
 })
-scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.chestOpen, function (sprite, location) {
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.chestOpen, function on_overlap_tile2(sprite5: Sprite, location2: tiles.Location) {
+    
     if (controller.A.isPressed()) {
         if (current_zone == "mz_1") {
             on_dialogue = true
-            story.startCutscene(function () {
+            story.startCutscene(function on_start_cutscene() {
+                
                 story.spriteSayText(player_1, "Already open. . .", 1, 15, story.TextSpeed.Slow)
                 story.spriteSayText(player_1, "Obviously.", 1, 15, story.TextSpeed.Slow)
                 story.spriteSayText(player_1, "God forbid I find something useful.", 1, 15, story.TextSpeed.Slow)
@@ -130,40 +140,53 @@ scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.chestOpen, function (spri
                 on_dialogue = false
             })
         }
+        
     }
+    
 })
-let bullet: Sprite = null
+let bullet : Sprite = null
 let x_direction = 0
 let y_direction = 0
 let on_dialogue = false
-let player_1: Sprite = null
+let player_1 : Sprite = null
 let current_zone = ""
-let map_zone_2: tiles.TileMapData = null
-let map_zone_1: tiles.TileMapData = null
-map_zone_1 = tilemap`mz_1`
-map_zone_2 = tilemap`mz_2`
+let map_zone_2 : tiles.TileMapData = null
+let map_zone_1 : tiles.TileMapData = null
+map_zone_1 = tilemap`
+    mz_1
+    `
+map_zone_2 = tilemap`
+    mz_2
+    `
 current_zone = "mz_1"
 tiles.setCurrentTilemap(map_zone_1)
-player_1 = sprites.create(assets.image`walk_1`, SpriteKind.Player)
+player_1 = sprites.create(assets.image`
+    walk_1
+    `, SpriteKind.Player)
 info.setLife(1)
 on_dialogue = false
 tileUtil.connectMaps(map_zone_1, map_zone_2, MapConnectionKind.Door1)
 tiles.placeOnRandomTile(player_1, sprites.dungeon.collectibleInsignia)
 let player_speed = 200
 controller.moveSprite(player_1, 100, 100)
-game.onUpdate(function () {
+game.onUpdate(function on_on_update() {
     if (on_dialogue) {
         controller.moveSprite(player_1, 0, 0)
         animation.stopAnimation(animation.AnimationTypes.All, player_1)
     } else {
         controller.moveSprite(player_1, 100, 100)
     }
+    
 })
-game.onUpdateInterval(5000, function () {
-    tileUtil.createSpritesOnTiles(sprites.dungeon.collectibleRedCrystal, assets.image`Ghost`, SpriteKind.Enemy)
-    tileUtil.createSpritesOnTiles(sprites.dungeon.collectibleBlueCrystal, assets.image`Ghost`, SpriteKind.Enemy)
+game.onUpdateInterval(5000, function on_update_interval() {
+    tileUtil.createSpritesOnTiles(sprites.dungeon.collectibleRedCrystal, assets.image`
+            Ghost
+            `, SpriteKind.Enemy)
+    tileUtil.createSpritesOnTiles(sprites.dungeon.collectibleBlueCrystal, assets.image`
+            Ghost
+            `, SpriteKind.Enemy)
 })
-forever(function () {
+forever(function on_forever() {
     scene.centerCameraAt(0, 0)
     scene.cameraFollowSprite(player_1)
     music.play(music.stringPlayable("C5 A B G A F G E ", 147), music.PlaybackMode.UntilDone)
