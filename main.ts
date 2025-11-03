f0c76e36963732d3b817cedf04829b1b8c04f9cb
Orlando Chirinos <153592294+OrlandoChir@users.noreply.github.com>
2025-11-03 21:06:01 +0100

    Backup

--- main.ts
+++ main.ts
@@ -1,7 +1,12 @@
 namespace SpriteKind {
     export const Dialogue = SpriteKind.create()
 }
+sprites.onCreated(SpriteKind.Enemy, function (sprite) {
+    sprite.follow(player_1, 30)
+})
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    y_direction = -200
+    x_direction = 0
     animation.runImageAnimation(
     player_1,
     assets.animation`walk_up_animation`,
@@ -11,18 +16,40 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 scene.onOverlapTile(SpriteKind.Player, tileUtil.door0, function (sprite, location) {
     tileUtil.loadConnectedMap(MapConnectionKind.Door1)
-    tiles.placeOnRandomTile(player_1, tileUtil.door0)
+    tiles.placeOnRandomTile(player_1, assets.tile`transparency16`)
     if (tileUtil.currentTilemap() == map_zone_2) {
         player_1.x = 26
     } else {
         player_1.x = 231
     }
 })
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 4 4 . . . . . . . 
+        . . . . . . 4 5 5 4 . . . . . . 
+        . . . . . . 2 5 5 2 . . . . . . 
+        . . . . . . . 2 2 . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, player_1, x_direction, y_direction)
+})
 controller.down.onEvent(ControllerButtonEvent.Released, function () {
     animation.stopAnimation(animation.AnimationTypes.All, player_1)
     player_1.setImage(assets.image`walk_down_1`)
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    y_direction = 0
+    x_direction = -200
     animation.runImageAnimation(
     player_1,
     assets.animation`WalkLeft`,
@@ -39,6 +66,8 @@ controller.left.onEvent(ControllerButtonEvent.Released, function () {
     player_1.setImage(assets.image`walk_left_1`)
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    y_direction = 0
+    x_direction = 200
     animation.runImageAnimation(
     player_1,
     assets.animation`WalkRight`,
@@ -50,7 +79,17 @@ controller.up.onEvent(ControllerButtonEvent.Released, function () {
     animation.stopAnimation(animation.AnimationTypes.All, player_1)
     player_1.setImage(assets.image`walk_up_1`)
 })
+tileUtil.onMapLoaded(function (tilemap2) {
+    sprites.destroyAllSpritesOfKind(SpriteKind.Enemy)
+    if (tileUtil.currentTilemap() == map_zone_1) {
+        tileUtil.coverAllTiles(tileUtil.door0, assets.tile`transparency16`)
+    } else {
+        tileUtil.coverAllTiles(tileUtil.door0, assets.tile`transparency16`)
+    }
+})
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    y_direction = 200
+    x_direction = 0
     animation.runImageAnimation(
     player_1,
     assets.animation`walk_down_animation`,
@@ -72,11 +111,19 @@ scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.chestOpen, function (spri
         }
     }
 })
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprites.destroy(ghost_1, effects.spray, 500)
+})
+let ghost_1: Sprite = null
+let projectile: Sprite = null
+let x_direction = 0
+let y_direction = 0
 let on_dialogue = false
 let player_1: Sprite = null
 let current_zone = ""
 let map_zone_2: tiles.TileMapData = null
-let map_zone_1 = tilemap`mz_1`
+let map_zone_1: tiles.TileMapData = null
+map_zone_1 = tilemap`mz_1`
 map_zone_2 = tilemap`mz_2`
 current_zone = "mz_1"
 tiles.setCurrentTilemap(map_zone_1)
@@ -98,4 +145,33 @@ forever(function () {
     scene.centerCameraAt(0, 0)
     scene.cameraFollowSprite(player_1)
     music.play(music.stringPlayable("C5 A B G A F G E ", 147), music.PlaybackMode.UntilDone)
+})
+game.onUpdateInterval(10000, function () {
+    ghost_1 = sprites.create(img`
+        ........................
+        ........................
+        ........................
+        ........................
+        ..........ffff..........
+        ........ff1111ff........
+        .......fb111111bf.......
+        .......f11111111f.......
+        ......fd11111111df......
+        ......fd11111111df......
+        ......fddd1111dddf......
+        ......fbdbfddfbdbf......
+        ......fcdcf11fcdcf......
+        .......fb111111bf.......
+        ......fffcdb1bdffff.....
+        ....fc111cbfbfc111cf....
+        ....f1b1b1ffff1b1b1f....
+        ....fbfbffffffbfbfbf....
+        .........ffffff.........
+        ...........fff..........
+        ........................
+        ........................
+        ........................
+        ........................
+        `, SpriteKind.Enemy)
+    tileUtil.createSpritesOnTiles(sprites.dungeon.collectibleRedCrystal, assets.image`Ghost`, SpriteKind.Enemy)
 })
